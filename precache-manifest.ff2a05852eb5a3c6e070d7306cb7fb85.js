77dd996a84c2c441a3c5db98a7b890833ce8c721
晨曦 <78677516+xcx2006@users.noreply.github.com>
2022-05-13 16:44:02 +0800

    Add files via upload

---
+++ precache-manifest.ff2a05852eb5a3c6e070d7306cb7fb85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7391bcebc6b191d11b",
+    "url": "css/app.45b03530.css"
+  },
+  {
+    "revision": "177c641f612092b8ba4e",
+    "url": "css/chunk-vendors.d39e9fad.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "18ecdaa43563964a922e3b916e1ddbd7",
+    "url": "index.html"
+  },
+  {
+    "revision": "0f7391bcebc6b191d11b",
+    "url": "js/app.c8793c39.js"
+  },
+  {
+    "revision": "177c641f612092b8ba4e",
+    "url": "js/chunk-vendors.d32e8171.js"
+  },
+  {
+    "revision": "28f3e13ec88073aa1b85b7d66358f613",
+    "url": "manifest.json"
+  }
+]);
